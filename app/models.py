ec647fce3c5fad3f87170206b3dcd7478a526695
suyiyun <su_yiyun@outlook.com>
2021-08-02 22:57:17 -0400

    module 6 update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Float, String, Integer, Numeric
 from .database import Base
 class salary(Base):
     __tablename__ = "salary"
-    id = Column(Integer, primarykey=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     player = Column(String, unique=True, index=True)
     fieldposition = Column(String)
     team = Column(String)
