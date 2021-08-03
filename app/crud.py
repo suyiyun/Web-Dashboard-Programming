2eefbf8fbe2f1a222b287de42ad7f2c857a9aa54
suyiyun <su_yiyun@outlook.com>
2021-08-02 22:53:50 -0400

    module 6 update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,4 @@
 from sqlalchemy.orm import Session
 from .import models
-def getsalary(db: Session):
+def get_salary(db: Session):
     returndb.query(models.salary.player, models.salary.fieldposition, models.salary.team).all()
