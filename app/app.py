e9b6b61f7cf8e6ef71132a1e4e636db6a5bb32ea
suyiyun <su_yiyun@outlook.com>
2021-08-04 23:41:25 -0400

    module 7 Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from sqlalchemy.orm import Session
 
 from . import crud, models
 from .database import SessionLocal, engine
 
-models.Base.metadata.create_all(bind=engine)
-app=FastAPI()
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import plotly_express as px
+import pandas as pd
+
+models.Base.metadata.create_all(bind = engine)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+templates = Jinja2Templates(directory = "templates")
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -16,6 +26,7 @@ def get_db():
 
 
 @app.get("/")
-def welcome(db: Session=Depends(get_db)):
-    x=crud.get_salary(db)
-    return x
+async def welcome(request: Request, db: Session = Depends(get_db)):
+    x = crud.get_salary(db)
+    a = 5
+    return templates.TemplateResponse("chart.html",{"request": request, "a": a})
