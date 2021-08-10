4e0b067077319f6c2007e498b002cdc3f2b91677
suyiyun <su_yiyun@outlook.com>
2021-08-09 23:40:19 -0400

    module 8 update app.py to match the div grids

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-import plotly_express as px
+import plotly.express as px
 import pandas as pd
 
 models.Base.metadata.create_all(bind = engine)
@@ -29,14 +29,25 @@ def get_db():
 async def welcome(request: Request, db: Session = Depends(get_db)):
     x = crud.get_salary(db)
     df = pd.DataFrame.from_records(x, columns = ['Player', 'FieldPosition', 'Team', 'Salary'])
-    df10 = df.head(10)
-    fig = px.bar(df10, x = 'Player', y = 'Salary', title = 'Top 10 Paid NFL Players')
+
+    px.defaults.width = 266
+    px.defaults.height = 200
+
+    fig = px.bar(df.head(10), x = 'Player', y = 'Salary', title = 'Top 10 Paid NFL Players')
+    fig.update_layout(yaxis = dict(tickfont = dict(size = 5)),
+    xaxis = dict(tickfont = dict(size = 5)),
+    font = dict(size = 5),
+    margin = dict(l = 0, r = 0, b = 0))
     top10 = fig.to_html(full_html = False, include_plotlyjs = 'cdn')
 
     dfteam = df.groupby('Team')['Salary'].sum()
     dfteam = dfteam.reset_index()
     dfteam = dfteam.sort_values('Salary', ascending = False).head(10)
     fig10 = px.bar(dfteam, x = 'Team', y = 'Salary', title = 'Top 10 Paying Teams')
+    fig10.update_layout(yaxis = dict(tickfont = dict(size = 5)),
+    xaxis = dict(tickfont = dict(size = 5)),
+    font = dict(size = 5),
+    margin = dict(l = 0, r = 0, t = 0, b = 0))
     team10 = fig10.to_html(full_html = False, include_plotlyjs = 'cdn')
 
     return templates.TemplateResponse("chart.html",{"request": request, "top10": top10, "team10": team10})
